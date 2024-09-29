26dbb1c07bf8c926037b58d96bf7570f433df663
文心 <121679359+LineXic@users.noreply.github.com>
2024-09-29 17:15:05 +0800

    Add files via upload

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,1 +1,30 @@
+<template>
+  <div class="about">
+    <h1>关于我</h1><br>
+  </div>
+  <p>这里是LineXic书屋，欢迎查看书屋，我是站长LineXic😀👆
+<br><br>
+我是LineXic，是一名初三的学生平时喜欢打羽毛球🏸和跑步🏃放假的时候喜欢在网络上瞎折腾
+<br><br>
+机遇巧合之下我喜欢上了编码并且投入的时间越来越长以至于了解的更多了，学到的多了也就想找个地方把它记录下来，后来我就了解到了GitHub
+<br><br>
+GitHub是一个储存代码的地方我可以把我写到的放在里面，通过注释的方法来帮我了解这串代码到底怎么用，怎么讲，只是后来我接触到了GitHub pages了解到了jekyll等静态网站搭建的博客，从此就开始了折腾之旅....</p>
+<br>
+<h1>联系我</h1>
+<br>
+<li>
+<p>
+<a href="https://github.com/LineXic">GitHub</a></p></li>
+<li>QQ:<a href="https://res.abeim.cn/api/qq/?qq=2793151375">2793151375</a></li>
+<li>邮箱:<a href="mailto:linexic@yeah.net">linexic@yeah.net</a></li>
+</template>
 
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
