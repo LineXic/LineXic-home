fd451fb6d4c324dec7ffda4218dbf4422f25b01d
文心 <121679359+LineXic@users.noreply.github.com>
2024-09-29 22:57:19 +0800

    更新 JiedanView.vue

--- src/views/JiedanView.vue
+++ src/views/JiedanView.vue
@@ -2,10 +2,28 @@
   <div class="jiedan">
     <h1>接单</h1><br>
   </div>
-                  
+    <h2>汉化接单</h2><br>
+    <p>我在<a href="https://klpbbs.com/">Minecraft-苦力怕论坛</a>发过几个Addon的汉化作品，这是我的两个号的链接：</p>
+    <li><a href="https://klpbbs.com/?1114580" target="_blank">LineXic</a></li>
+    <li><a href="https://klpbbs.com/?686976" target="_blank">LineXics</a></li>
+    <br>
+    <p>这两个账号里面都是我的汉化作品，其中「LineXics」不更新了，也就是说弃号了，「LineXic」还在活跃，这是我的接单介绍</p>
+  <img id="img"src="https://chengai77a6b.top/linexic.jpg" alt="接单"/>
+  <h2>其他</h2>
+  <br>
+  <p>另外我也可以搭建相关官网网站，比如服务器的文档，个人的博客等等，可以使用vue和vite框架搭建，现在你看到的站点就是我用vue框架搭建的
+  <br><br>
+  也可以搭建一些服务器博客，如typecho和halo等，这些酌情收费<a href="https://chengai77a6b.top/" target="_blank">承挨汉化组官网</a>及自己的个人主页就是我搭建并部署到GitHub上的，关于我的其他信息也可以在<a href="https://blog.linexic.top/about" target="_blank">LineXic博客|关于我</a>找到
+  </p>
+  <h1>联系我</h1>
+  <li><a href="https://res.abeim.cn/api/qq/?qq=2793151375" target="_blank">QQ</a></li>
+  <li><a href="mailto:linexic@yeah.net" target="_blank">邮箱</a></li>
+  <li><a href="https://b23.tv/aKF3CVt" target="_blank">bilibili</a></li>
 </template>
-
 <style>
+#img{
+   width: 100%;
+}
 @media (min-width: 1024px) {
   .jiedan {
     min-height: 100vh;
