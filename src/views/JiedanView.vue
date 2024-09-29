9b13467e514263e8401465d186208538e5bdd14f
文心 <121679359+LineXic@users.noreply.github.com>
2024-09-29 22:16:22 +0800

    更新 views

---
+++ src/views/JiedanView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jiedan">
+    <h1>接单</h1><br>
+  </div>
+                  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .jiedan {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
